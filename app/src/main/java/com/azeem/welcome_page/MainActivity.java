46f34ac21493fffc79bb957203de7889296b8078
Azeem-Raza <azeemraza2001@gmail.com>
2024-09-18 13:33:07 +0300

    Changed the display placeholder

--- app/src/main/java/com/azeem/welcome_page/MainActivity.java
+++ app/src/main/java/com/azeem/welcome_page/MainActivity.java
@@ -1,6 +1,6 @@
 package com.azeem.welcome_page;
 
-import android.os.Bundle;
+import android.os.Bundle; // Imports the Bundle class, which is used to pass data between Android components.
 import android.app.Activity;
 import android.view.Menu;
 
